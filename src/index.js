5c76188f78fe6b58d59cc633d2d9c35920043e20
UPADHYAY-YATENDRA <yupadhyayyk@gmail.com>
2023-12-18 13:57:26 +0530

    Google analytics

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from "react";
-import { logPageView } from "./analytics";
+import { initGA, logPageView } from './analytics';
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+// Initialize Google Analytics
+initGA();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const RootComponent = () => {
