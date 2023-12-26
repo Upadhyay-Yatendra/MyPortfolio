e4d8231a5445f0bf8bfbbb4dac5cabbf5fe73f28
UPADHYAY-YATENDRA <yupadhyayyk@gmail.com>
2023-12-27 01:29:44 +0530

    Improved About section

--- src/components/Techstack.js
+++ src/components/Techstack.js
@@ -7,7 +7,7 @@ import {
   FaBootstrap,
   FaWordpress,
   FaNode,
-  FaLinux
+  FaLinux,
 } from "react-icons/fa";
 import { BiLogoJavascript } from "react-icons/bi";
 import { TbBrandOauth } from "react-icons/tb";
@@ -19,82 +19,114 @@ import {
   SiJquery,
   SiPwa,
   SiExpress,
-  SiMongodb
+  SiMongodb,
 } from "react-icons/si";
 import { BsGit } from "react-icons/bs";
 
 const data = [
   {
     icon: <AiFillHtml5 fontSize={70} color="#fff" />,
+    name: "HTML5",
   },
   {
     icon: <FaCss3Alt fontSize={70} color="#fff" />,
+    name: "Css3",
   },
   {
     icon: <BiLogoJavascript fontSize={70} color="#fff" />,
+    name: "Javascript",
   },
-  {
-    icon: <SiFirebase fontSize={70} color="#fff" />,
-  },
-  {
-    icon: <BsGit fontSize={70} color="#fff" />,
-  },
-  {
-    icon: <AiFillGithub fontSize={70} color="#fff" />,
-  },
+
   {
     icon: <FaReact fontSize={70} color="#fff" />,
+    name: "React",
   },
-  {
-    icon: <SiRedux fontSize={70} color="#fff" />,
-  },
+
   {
     icon: <SiTailwindcss fontSize={70} color="#fff" />,
+    name: "Tailwind",
   },
   {
     icon: <FaBootstrap fontSize={70} color="#fff" />,
+    name: "Bootstrap",
   },
   {
     icon: <SiJquery fontSize={70} color="#fff" />,
+    name: "JQuery",
+  },
+  {
+    icon: <SiFirebase fontSize={70} color="#fff" />,
+    name: "Firebase",
+  },
+  {
+    icon: <BsGit fontSize={70} color="#fff" />,
+    name: "Git",
+  },
+  {
+    icon: <AiFillGithub fontSize={70} color="#fff" />,
+    name: "Github",
   },
   {
     icon: <SiPwa fontSize={70} color="#fff" />,
+    name: "PWA",
   },
   {
     icon: <FaNode fontSize={70} color="#fff" />,
+    name: "Node",
   },
   {
     icon: <SiExpress fontSize={70} color="#fff" />,
+    name: "Express",
   },
   {
     icon: <SiMongodb fontSize={70} color="#fff" />,
-  }, 
-  {
-    icon: <SiGreensock fontSize={70} color="#fff" />,
+    name: "MongoDb",
   },
   {
     icon: <FaLinux fontSize={70} color="#fff" />,
-  },
-  
-  {
-    icon: <TbBrandOauth fontSize={70} color="#fff" />,
+    name: "Linux",
   },
 
+  {
+    icon: <TbBrandOauth fontSize={70} color="#fff" />,
+    name: "Oauth",
+  },
 ];
 function Techstack() {
   return (
-    <div className="relative w-11/12 lg:w-4/5 mx-auto flex items-center justify-center gap-3 flex-wrap my-5 lg:my-10">
+    <div className="relative w-11/12 lg:w-4/5 mx-auto flex items-center justify-center gap-3 flex-wrap my-5">
       <img
         src="./star.jpg"
         alt=""
         className="h-full w-full object-cover opacity-10 absolute bottom-0"
       />
       {data.map((item, i) => (
-        <div
-          className="w-36 md:w-60 h-36 flex items-center justify-center m-3 border border-[#df8de2] bg-black/30  z-20 rounded-sm hover:border-[#ad50eb] hover:scale-[1.02] transition-all duration-300"
-          key={i}
-        >
-          {item.icon}
+        <div className="text-center" key={i}>
+          <div className="group">
+            <div
+              className="w-36 md:w-60 h-36 flex items-center justify-center m-3 border border-[#df8de2] bg-black/30 z-20 rounded-sm transition-all duration-300 transform hover:scale-105"
+              key={i}
+              style={{
+                borderColor: "#df8de2", // Initial border color
+              }}
+              onMouseEnter={(e) => {
+                e.currentTarget.style.borderColor = "#ad50eb"; // Purple border on hover
+              }}
+              onMouseLeave={(e) => {
+                e.currentTarget.style.borderColor = "#df8de2"; // Reset to initial border color
+              }}
+            >
+              <div
+                className="border border-transparent"
+                style={{ transition: "border-color 0.3s" }}
+              >
+                {item.icon}
+              </div>
+            </div>
+            <p className="text-white opacity-0 group-hover:opacity-100">
+              {item.name}
+            </p>
+          </div>
         </div>
       ))}
     </div>
--- src/components/Toolstack.js
+++ src/components/Toolstack.js
@@ -5,26 +5,37 @@ import {
   SiPostman,
   SiVercel,
   SiNetlify,
-  SiRender
+  SiRender,
 } from "react-icons/si";
 function Toolstack() {
   const data = [
     {
       icon: <BsWindows fontSize={70} color="#fff" />,
+      name: "Windows",
     },
     {
       icon: <SiVisualstudiocode fontSize={70} color="#fff" />,
+      name: "VSCode",
     },
     {
       icon: <SiPostman fontSize={70} color="#fff" />,
+      name: "Postman",
     },
     {
-      icon: <SiNetlify fontSize={70} color="#fff" />,
+      icon: <SiVercel fontSize={70} color="#fff" />,
+      name: "Vercel",
     },
     {
       icon: <SiRender fontSize={70} color="#fff" />,
-    }
+      name: "Render",
+    },
   ];
+
+  const textStyle = {
+    color: "white",
+    marginTop: "0.5rem",
+  };
+
   return (
     <div className="relative w-11/12 lg:w-4/5 mx-auto flex items-center justify-center gap-3 flex-wrap my-5">
       <img
@@ -33,11 +44,32 @@ function Toolstack() {
         className="h-full w-full object-cover opacity-10 absolute bottom-0"
       />
       {data.map((item, i) => (
-        <div
-          className="w-36 md:w-60 h-36 flex items-center justify-center m-3 border border-[#df8de2] bg-black/30  z-20 rounded-sm hover:border-[#ad50eb] hover:scale-[1.02] transition-all duration-300"
-          key={i}
-        >
-          {item.icon}
+        <div className="text-center" key={i}>
+          <div className="group">
+            <div
+              className="w-36 md:w-60 h-36 flex items-center justify-center m-3 border border-[#df8de2] bg-black/30 z-20 rounded-sm transition-all duration-300 transform hover:scale-105"
+              key={i}
+              style={{
+                borderColor: "#df8de2", // Initial border color
+              }}
+              onMouseEnter={(e) => {
+                e.currentTarget.style.borderColor = "#ad50eb"; // Purple border on hover
+              }}
+              onMouseLeave={(e) => {
+                e.currentTarget.style.borderColor = "#df8de2"; // Reset to initial border color
+              }}
+            >
+              <div
+                className="border border-transparent"
+                style={{ transition: "border-color 0.3s" }}
+              >
+                {item.icon}
+              </div>
+            </div>
+            <p className="text-white opacity-0 group-hover:opacity-100">
+              {item.name}
+            </p>
+          </div>
         </div>
       ))}
     </div>
